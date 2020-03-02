07c530ab01b150761a4249f294ab198a2e51ac0f
vivianeluzz <vivianesluzz@gmail.com>
2020-03-02 16:53:44 -0300

    exercicio de variaveis

---
+++ semana3/logica-booleana/js-vanilla-template/src/index.js
@@ -0,0 +1,57 @@
+// Exercício 1
+// a.  false
+// b.  false
+// c.  true
+// d.  false
+// e.  boolean
+
+// Exercício 2
+
+// a. O que é `array` e como se declara em `JS`?
+// São formas de  armazenar várias informações entre colchetes []
+// b. Qual o index inicial de um `array`?
+// let array
+// c. Como se determinar o tamanho do `array`?
+// array.length
+// d. Indique todas as mensagens impressas no console.
+
+// I.  undefined
+// II.  null
+// III.  11
+// IV.  3  e  4
+// V.  19  e  9
+// VI.  3
+// VII.  1
+
+
+
+const rua = prompt("qual o nome da rua?")
+const cor = prompt("a sua cor favorita?") 
+let idade = prompt("Qual a sua idade?")
+let idade = 32
+const estuda = prompt("o que voce estuda?")
+
+
+
+
+         const valor = prompt ("qual a temperatura?")
+        const calculo = Number(valor - 32)*5/9 + 273.15
+        const resultadoKelvin = calculo + "k"
+        console.log(resultadoKelvin)
+
+    const valor = prompt ("qual a temperatura?")
+    const calculo = Number(valor)*9/5 + 32
+    const resultadoFahrenheit = calculo + "F"
+    console.log(resultadoFahrenheit)
+
+    const valor = prompt ("qual a temperatura?")
+    const calculoFahrenheit = Number(valor)*9/5 + 32
+    const calculoKelvin = Number(valor - 32)*5/9 + 273.15
+    const resultadoFahrenheit = calculoFahrenheit + "F"
+    const resultadoKelvin = calculoKelvin + "K"
+    console.log(resultadoFahrenheit, resultadoKelvin)
+
+
+
+
+    
