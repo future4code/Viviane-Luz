dbf8c04dd6ea80e64654ccd1138da8463feb28e5
vivianeluzz <vivianesluzz@gmail.com>
2020-03-10 16:16:17 -0300

    Adicionando Planner

---
+++ semana4/planner/index.js
@@ -0,0 +1,7 @@
+function diaDaSemana(){
+    const input = document.querySelector("input")
+    const selecionaDiaTarefa = input.value
+    const tarefaPlanner = document.querySelector("diaDaSemana")
+    const divsegunda = document.getElementById("segunda")
+    divsegunda.inner +="<div>" + selecionarTarefa + "</div>"
+}
