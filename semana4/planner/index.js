b6906de5f243886783cb7aa8384ad273a9742569
vivianeluzz <vivianesluzz@gmail.com>
2020-03-10 17:05:28 -0300

    criação planner

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -3,5 +3,20 @@ function diaDaSemana(){
     const selecionaDiaTarefa = input.value
     const tarefaPlanner = document.querySelector("diaDaSemana")
     const divsegunda = document.getElementById("segunda")
-    divsegunda.inner +="<div>" + selecionarTarefa + "</div>"
+    divsegunda.innerHTML +="<div>" + selecionaDiaTarefa + "</div>"
+    const divterca = document.getElementById("terca")
+    divterca.innerHTML +="<div>" + selecionaDiaTarefa + "</div>"
+    const divquarta = document.getElementById("quarta")
+    divquarta.innerHTML +="<div>" + selecionaDiaTarefa + "</div>"
+    const divquinta = document.getElementById("quinta")
+    divquinta.innerHTML +="<div>" + selecionaDiaTarefa + "</div>"
+    const divsexta = document.getElementById("sexta")
+    divsexta.innerHTML +="<div>" + selecionaDiaTarefa + "</div>"
+    const divsabado = document.getElementById("sabado")
+    divsabado.innerHTML +="<div>" + selecionaDiaTarefa + "</div>"
+    const divdomingo = document.getElementById("domingo")
+    divdomingo.innerHTML +="<div>" + selecionaDiaTarefa + "</div>"
+
+
+
 }
