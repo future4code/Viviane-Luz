d854d56e6c3656971753f6e7f36a2ba08609e662
vivianeluzz <vivianesluzz@gmail.com>
2020-03-12 17:05:00 -0300

    Atividade controlador de despesas

---
+++ semana4/funcoes-de-array/controlador-de-despesas/index.js
@@ -0,0 +1,35 @@
+class Despesa{
+    constructor(valor,tipoDespesa,descricao){
+    this.valor = valor;
+    this.tipoDespesa = tipoDespesa;
+    this.descricao = descricao
+}
+
+}
+const banco = [];
+
+function cadastreDespesa(){
+    const valor = document.querySelectorById("valor");
+    const selecionaValor = input.value
+    const tipodespesa = document.getElementById("despesa");
+    const despesa = input.value
+    const descricao = document.getElementById("descricao");
+    const descricao = input.value
+
+    console.log()
+}
+
+input.value = ""
+
+     if( despesa === null || selecionaDiaTarefa === ""){
+         alert("Sem valor!")
+    
+
+     input.value = ""
+ }
+
+//  const filtrarDespesa=[
+//      { casa:"", festas:"", viagens:""
+
+//      }
+//  ]
