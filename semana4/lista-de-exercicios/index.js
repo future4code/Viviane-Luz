0472b9b3beb412d1b43f8a9ad9d9b488f6bb6d59
vivianeluzz <vivianesluzz@gmail.com>
2020-03-13 17:09:07 -0300

    lista de exercicios

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,114 @@
+// Exercícios de Lógica de Programação
+/*
+1.
+let i = 0
+while(i < 8){
+    console.log(i)
+    i++
+}
+
+
+for (let i = 0; i < 10; i++){
+    console.log(i)
+}
+
+
+
+2. a)False, b) True, c) True,   d)True, e)True
+*/
+
+// 3.
+// const quantidadeDeNumerosPares = (0, 2, 4, 5, 6, 8)
+// let i = 0
+// while(i <= quantidadeDeNumerosPares) {
+//   console.log(i*2)
+// if(i%2 === 0){
+//     console.log(i)
+// }
+// }
+// Não funciona, porque a constante não está declarada.
+
+
+// - Exercícios de Funções
+// 5-
+// function verificaMaiorNumero(numero1, numero2){
+//     const numero1 = numero2()
+
+
+//     if(numero1 < numero2){
+//         console.log("maior")
+//     }
+//      if(numero1%2 === 0){
+//         console.log("é divisível")
+//     }
+// }
+
+// console.log(15,30)
+
+
+// - Exercícios de Funções
+//  1.   
+// const array = [0, 2, 4, 14, 25, 25, 30, 55]
+// let segundoMenor= 0;
+// let segundoMenor = 0;
+
+// if(segundoMenor === 2){
+//     console.log("segundoMenor")
+// }
+// else if (segundoMaior === 30){
+//     console.log("segundoMaior")
+// }
+
+
+// // 2-
+// let verificaCurso = (curso)=>{
+//     if(curso === Future4){
+//         alert("Hello Future4")
+//     }
+// }
+
+/* Exercícios de Objetos
+Um array é quando podemos declarar vários elementos dentro de uma variável e pode fazer modelagens em objetos. 
+    
+
+
+
+
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
