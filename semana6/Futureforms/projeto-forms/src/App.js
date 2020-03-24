310d3c1f491d6f2e3eb6166f8a435a265b2d6d75
vivianeluzz <vivianesluzz@gmail.com>
2020-03-24 13:10:13 -0300

    adicionado e removido pasta

--- semana6/Futureforms/projeto-forms/src/App.js
+++ semana6/Futureforms/projeto-forms/src/App.js
@@ -1,18 +1,56 @@
-import React from 'react';
-import Botao from './Components/Botao/Botao'
-import Input from './Components/Input/input'
-// import './App.css';
+import React from "react";
+import Form1 from "./Components/Form1/Form1";
+import Form2 from "./Components/Form2/Form2";
+import Form3 from "./Components/Form3/Form3";
+import Form4 from "./Components/Form4/Form4";
+import { render } from '@testing-library/react';
+import './App.css';
 
-function App() {
-  return(
-    <div>
-      <Botao />
-      <Input />
-    </div>
-  )
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      etapa: 0
+    };
+  
 
+  somaClicks = () => {
+  const proximaEtapa = this.state.etapa + 1;
+  this.setState({
+    etapa: proximaEtapa
+  });
+  };
 
+  render(){
+  let etapajsx = null;
 
-
-export default App
+  switch (this.state.etapa) {
+      case 0:
+      etapajsx = <Form1 />;
+      break;
+      case 1:
+      etapajsx = <Form2 />;
+      break;
+      case 2:
+      etapajsx = <Form3 />;
+      break;
+      case 3:
+      etapajsx = <Form4 />;
+      break;
+      default:
+      return (
+      <div>
+        {etapajsx}
+        {this.state.etapa < 3 ? (
+          <button onClick={this.somaClicks}>Próxima etapa</button> 
+        ) : (
+          <Form4 />
+        )
+          }
+      </div>
+      );
+        }
+  }
+  }
+ 
+export default App;
