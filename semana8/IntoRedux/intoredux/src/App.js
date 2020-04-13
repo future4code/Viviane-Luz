9aa680040d43ee8f4d88bc52c684dcf39bc3ef9f
vivianeluzz <vivianesluzz@gmail.com>
2020-04-13 17:04:26 -0300

    exercício action react-redux

---
+++ semana8/IntoRedux/intoredux/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Task from './Components/Task'
+import Funções from './Components/Funções'
+
+
+
+function App() {
+  return (
+    <div>
+       <Task />
+    </div>
+  );
+}
+
+export default App;
---
+++ semana8/IntoRedux/intoredux/src/Components/Funções.js
@@ -0,0 +1,46 @@
+// import React from 'react';
+// import PropTypes from 'prop-types'
+// import styled from 'styled-components'
+// import axios from 'axios'
+
+
+// class Funções extends React.Component {
+//     constructor(props){
+//         super(props) 
+//     }
+// }
+
+// function addTask() {
+//     return {
+//         type: 'ADICIONAR_TAREFA',
+//         payload: {
+//             valor: completas
+//         }
+
+//     }
+// }
+// function deleteTask() {
+//     return {
+//         type: 'Deletar_TAREFA',
+//         payload: {
+//             valor: pendentes
+//         }
+
+//     }
+// }
+// function deleteTask() {
+//     return {
+//         type: 'Marcar_TAREFA',
+//         payload: {
+//             valor: todas
+//         }
+
+//     }
+// }
+
+
+
+
+
+
+// export default Funções
