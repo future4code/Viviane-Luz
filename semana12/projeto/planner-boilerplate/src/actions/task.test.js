f5c718a106506c2101ffda1fc8103e8e1fc3b3bb
vivianeluzz <vivianesluzz@gmail.com>
2020-05-10 20:50:22 -0300

    testes de actions

--- semana12/projeto/planner-boilerplate/src/actions/task.test.js
+++ semana12/projeto/planner-boilerplate/src/actions/task.test.js
@@ -1,14 +1,70 @@
-import { setTask } from "./task";
+import { setTask, getTask, createTask } from "./task";
+import axios from "axios";
 
-describe("task", () => {
+describe("task", () => { 
     test("setTask", () => {
       // Preparação
-      const mockTodoText = "teste";
+      const mockSetTask = "teste";
     //  Execução 
-      const action = setTask(mockTodoText);
+      const action = setTask(mockSetTask);
 // Verificação
 expect(action.type).toBe("SET_TASKS");
-expect(action.payload.text).toBe(mockTodoText);
+expect(action.payload.task).toBe(mockSetTask);
 });
-})
+
+// teste2
+test ("Lista de teste", async () => {
+  // mock dos axios
+  axios.get = jest.fn(async () => ({
+    data:[
+      {
+        text: "teste2",
+        day: "teste2"
+      }
+    ]
+  }))
+   
+  // mock do dispatch 
+  const dispatch = jest.fn()
+
+  // executa ação assicrona
+  await getTask() (dispatch)
+
+    // verifica se o dispatch foi chamado a ação correta
+
+    expect(dispatch).toHaveBeenCalledWith({
+      type: "SET_TASKS",
+      payload: {
+        task: [
+          {
+            text: "teste2",
+            day: "teste2"
+          }
+        ]
+      }
+    })
+
+  })
+
+  test("Teste Create", async () => {
+    axios.post = jest.fn(async() =>{
+      const body = 
+      
+        {
+          text: "teste3",
+          day: "teste3",
+        }
+        const dispatch = jest.fn()
+        await createTask(body)(dispatch)
+        expect(dispatch).toHaveBeenCalledWith(getTask)
+    })
+      
+      
+  }
+  )
+}
+)
+// teste3
+
+
 
