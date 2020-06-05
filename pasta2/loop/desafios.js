0167e3e95e82fab5446ddab651b2ad28d35fe19f
vivianeluzz <vivianesluzz@gmail.com>
2020-06-04 23:37:15 -0300

    desafios

---
+++ pasta2/loop/desafios.js
@@ -0,0 +1,23 @@
+//DESAFIO 1
+/* console
+0
+00
+000
+0000
+*/
+
+//DESAFIO 2
+//Não consegui fazer um loop e nem a quantidade de vezes que foi de chutes.
+
+const numeroCerto = Number(prompt("Digite um numero"))
+console.log("vamos jogar")
+
+
+const numeroChutado = Number(prompt("Chute um numero"))
+
+if(numeroChutado > numeroCerto){   
+    console.log("Errou!!! o numero certo é menor que o chutado")
+
+}if(numeroChutado === numeroCerto)
+console.log("Acertou!!")
+
