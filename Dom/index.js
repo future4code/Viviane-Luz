5f6f6a797fcab9640b01d758b58d379a65b06432
vivianeluzz <vivianesluzz@gmail.com>
2020-06-09 20:26:25 -0300

    Finalizado

--- Dom/index.js
+++ Dom/index.js
@@ -3,18 +3,12 @@ function adicionarTarefa() {
     const tarefaDaSemana = document.getElementById("semana")
 
     let tarefa = meuInput.value
-    let semana = meuSelect.value
+    let semana = tarefaDaSemana.value
 
     
-    const tarefaSegunda  = document.getElementById("Segunda")
-    const tarefaTerça = document.getElementById("Terça")
-    const tarefaQuarta  = document.getElementById("Quarta")
-    const tarefaQuinta  = document.getElementById("Quinta")
-    const tarefaSexta  = document.getElementById("Sexta")
-    const tarefaSábado  = document.getElementById("Sábado")
-    const tarefaDomingo  = document.getElementById("Domingo")
-
-    tarefaSegunda.innerHTML += `<li> ${tarefa} </li>`
+    const elemento  = document.getElementById(semana)
+    
+    elemento.innerHTML += `<li> ${tarefa} </li>`
     meuInput.value = ""
     
     tarefaDaSemana.innerHTML += `<ul> ${semana} </ul>`
