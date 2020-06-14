3fd44cf3eaf4f17e988bbd1fd8c80d037140a75e
vivianeluzz <vivianesluzz@gmail.com>
2020-06-14 11:37:50 -0300

    Exercício Js

--- pasta2/ListaJs/index.js
+++ pasta2/ListaJs/index.js
@@ -82,8 +82,7 @@ function labenu (){
 labenu()
 
 //- Exercícios de Objetos
-//1. São vários valores guardados dentro de uma array e dados
-// e quando queremos acessar um valor dentro do objeto.
+//1. São vários valores guardados dentro de um array e quando queremos acessar um valor dentro do objeto.
 
 //2.
 
@@ -103,6 +102,13 @@ const filme = {
  console.log("Venha assitir ao filme" + ano + "dirigido por " + diretor + " " + "e estrelado por" + ator1);
   
 //4
+const nomePessoa = {
+    nome: "Viviane",
+    idade: 30,
+    email: "labenu@gmail.com",
+    endereco: "Marcondes de Andrade",
+}
+
 const anonimizarPessoa = {
    nome: "anonimo",
    idade: 30,
@@ -110,5 +116,146 @@ const anonimizarPessoa = {
    endereco: "Marcondes de Andrade",
     }
     
-console.log(anonimizarPessoa)
-    
+const anonimizarPessoa = nomePessoa.map((nomePessoa, index, array) => {
+    return {
+       nome: anonimizarPessoa.nome
+    } 
+})
+
+console.log(anonimizarPessoa);
+
+const pessoa = {
+    nome: 'Maria',
+    idade: 20,
+    email: 'maria@gmail.com',
+    endereco: 'Rua do Comercio, 21'
+}
+
+anonimizarPessoa = (pessoa) => {
+    return anonimo = {
+        ...pessoa,
+        nome: 'Anônimo',
+    }
+}
+
+const pessoaAnonima = anonimizarPessoa(pessoa);
+console.log(pessoaAnonima);
+//
+
+//Exercícios de Funções de array
+//a.
+const pessoas = 
+[
+	{ nome: "Pedro", idade: 20 },
+	{ nome: "João", idade: 10 },
+	{ nome: "Paula", idade: 12 },
+	{ nome: "Artur", idade: 89 } 
+]
+const idadeMaiorQueVinte = pessoas.filter((pessoa, index, array) => {
+    if(pessoa.idade > 20){
+        return {
+            nome: pessoa.nome,
+            idade: pessoa.idade
+        }
+    }
+    return false
+})
+
+console.log(idadeMaiorQueVinte)
+
+//b.
+const criancas = 
+[
+	{ nome: "Pedro", idade: 20 },
+	{ nome: "João", idade: 10 },
+	{ nome: "Paula", idade: 12 },
+	{ nome: "Artur", idade: 89 } 
+]
+
+const retornaCriancas = criancas.filter((crianca, index, array) => {
+    if(crianca.idade < 20){
+        return {
+            nome: crianca.nome,
+            idade: crianca.idade
+        }
+    }
+    return false
+})
+
+console.log(retornaCriancas)
+
+//3 
+//a.
+
+let arrayTeste = [1, 2, 3, 4, 5, 6]
+
+let multiplicaArray = arrayTeste.map((arrayTeste, index, array) => {
+    return arrayTeste * 2;
+})
+console.log(multiplicaArray)
+
+//b
+
+let arrayTeste = [1, 2, 3, 4, 5, 6]
+
+let multiplicaArray = arrayTeste.map((arrayTeste, index, array) => {
+    return arrayTeste * 3;
+})
+console.log(`${multiplicaArray}`)
+
+//c.
+
+let numeros = [1, 2, 3, 4, 5, 6]
+
+let array = numeros.map((numeros, index, array) => {
+    if(numeros % 2 === 0){
+        console.log(`${numeros} é par`)
+    }else{
+        console.log(`${numeros} é impar`)
+    }
+
+})
+console.log(numeros)
+
+//3 a.
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+
+const permissao = pessoas.filter((pessoa, index,array) => {
+if(pessoa.idade > 14 && pessoa.idade < 60 && pessoa.altura <= 1.5){
+    return {
+        nome: pessoa.nome,
+        idade: pessoa.idade,
+        altura: pessoa.altura
+    }
+}
+})
+console.log(permissao)
+
+//b.
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+
+const naoPodeEntrar = pessoas.filter((pessoa, index, array) => {
+    if(pessoa.idade <= 14 || pessoa.idade >= 60){
+        return {
+            pessoa
+        }
+    }
+})
+
+console.log(naoPodeEntrar)
+//
+
